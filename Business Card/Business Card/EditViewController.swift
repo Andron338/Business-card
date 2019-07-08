d7bad8adf2c4e884c1aee0331d157c38743c8ae4
Andron338 <nebesoandrian@gmail.com>
2019-07-08 19:06:44 +0300

    kek

--- Business Card/Business Card/EditViewController.swift
+++ Business Card/Business Card/EditViewController.swift
@@ -10,43 +10,18 @@ import UIKit
 
 class EditViewController: UIViewController {
 
-    @IBOutlet weak var phoneNumberTextField: UITextField!
-    @IBOutlet weak var dateTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.hideKeyboardWhenTappedAround()
+        
         // Do any additional setup after loading the view.
-        self.phoneNumberTextField.keyboardType = UIKeyboardType.decimalPad
-    }
-    @IBAction func dateTextField(_ sender: UITextField) {
-        let datePickerView:UIDatePicker = UIDatePicker()
-        datePickerView.datePickerMode = UIDatePicker.Mode.date
-        if dateTextField.text! != "" {
-            let dateSetFormatter = DateFormatter()
-            dateSetFormatter.dateFormat = "MMMM d, y"
-            datePickerView.date = dateSetFormatter.date(from: dateTextField.text!)!
-        }
-        dateTextField.inputView = datePickerView
-        datePickerView.addTarget(self, action: #selector(datePickerFromValueChanged), for: UIControl.Event.valueChanged)
-        }
-    @objc func datePickerFromValueChanged(sender:UIDatePicker) {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MMMM d, y"
-        dateTextField.text = dateFormatter.string(from: sender.date)
-    }
-}
-
-extension UIViewController {
-    func hideKeyboardWhenTappedAround() {
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
+        
     }
     
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
+    
 }
+
+
     
 
     
